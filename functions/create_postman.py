61a9ab741ed2b8412d5200c2b43e6989ab5b6b32
unknown <vitormolnar12@hotmail.com>
2023-09-21 21:06:58 -0300

    Atualização com a Main

--- functions/create_postman.py
+++ functions/create_postman.py
@@ -2,9 +2,6 @@ import requests
 import json
 import requests
 
-with open('curl.txt', 'r') as file:
-    curl_data = file.read()
-
 def create_collection(api_key, collection_name):
     url = "https://api.getpostman.com/collections"
 
@@ -67,10 +64,6 @@ def create_folder(api_key, collection_id, folder_name, collection_name):
     return response.json()
     
    
-# <<<<<<< HEAD
-# def create_request(api_key, collection_name, collection_id, folder_name, folder_id, request_name, request_method, request_body, request_url, request_headers, test_script):
-    
-# =======
 def create_request(api_key, collection_name, collection_id, folder_name, folder_id, request_name, request_method, request_headers, request_body, request_url, test_script):
 
     url = f"https://api.getpostman.com/collections/{collection_id}"
@@ -80,30 +73,11 @@ def create_request(api_key, collection_name, collection_id, folder_name, folder_
         "Content-Type": "application/json"
     }
 
-    # header_lines = curl_data.split('\n')
-    # headers = []
-
-    # for line in header_lines:
-    #     if ':' in line:
-    #         key, value = line.split(':', 1)
-    #         headers.append({"key": key.strip(), "value": value.strip()})
-
     request_item = {
         "name": request_name,
         "request": {
             "url": request_url,
-#             "method": request_method,
-# <<<<<<< HEAD
-#             #Header não está sendo pega. Caso coloque "Request Headers" da pau. Criar um for para separar "key"(Authorization) e "value"(Token)
-#             "request_headers": {
-#                 "key": "Content-Type", "value": "application/json",  # Exemplo de cabeçalho Content-Type
-#                 "key": "Authorization", "value": request_headers
-#             },
-#             "body": {
-#                 "mode": "raw",
-#                 "raw": json.dumps(request_body, indent=2)
-#         }
-# =======
+            "method": request_method,
             "header": request_headers,
         },
         "event": [
@@ -139,5 +113,6 @@ def create_request(api_key, collection_name, collection_id, folder_name, folder_
         }
     }
 
-    response = requests.put(url, headers=request_headers, json=data)
+    response = requests.put(url, headers=headers, json=data)
     return response.json()
+
--- main.py
+++ main.py
@@ -52,10 +52,6 @@ test_script = "console.log()"
 
 print("\n----------------------------------------------------------------------\n")
 
-# <<<<<<< HEAD
-# new_request = create_request(api_key, collection_name, collection_id, folder_name, folder_id, request_name, request_method, request_body, request_url, request_headers, test_script)
-# #print("\nNew Request created!\n")
-# =======
 request_headers = format_headers(headers_dict)
 
 new_request = create_request(api_key, collection_name, collection_id, folder_name, folder_id, request_name, request_method, request_headers, request_body, request_url, test_script)
