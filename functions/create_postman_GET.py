00c0d79ec0ef5a325e06e27923d6d76f5e8d4f3e
unknown <vitormolnar12@hotmail.com>
2024-04-01 10:10:53 -0300

    Att GET

---
+++ functions/create_postman_GET.py
@@ -0,0 +1,94 @@
+import requests
+import json
+test_script = 'console.log()'
+
+def read_curl_file(file_path):
+    with open(file_path, 'r') as file:
+        curl_command = file.read()
+    url_start = curl_command.find('"') + 1
+    url_end = curl_command.find('"', url_start)
+    url = curl_command[url_start:url_end]
+    return url
+
+def parse_query_params(url):
+    params = url.split('?')[1]
+    param_list = params.split('&')
+    parsed_params = {}
+    for param in param_list:
+        key, value = param.split('=')
+        parsed_params[key] = value
+    return parsed_params
+
+def edit_query_params(parsed_params):
+    edited_params = []
+    for key, value in parsed_params.items():
+        edited_value = value + '_test'
+        edited_params.append(f"{key}={edited_value}")
+    return "&".join(edited_params)
+
+def create_request(api_key, collection_id, folder_id, request_name, request_method, request_headers, request_body, request_url, test_script):
+    
+    url = f'https://api.getpostman.com/collections/{collection_id}/requests?folder={folder_id}'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'X-API-Key': api_key
+    }
+    if request_body is None:
+                data = {
+            "name": request_name,
+            "url": request_url,
+            "method": request_method,
+            "headers": request_headers,
+            "dataMode": "none",
+            "events": [
+                {
+                    "listen": "test",
+                    "script": {
+                        "id": "a8608e1a-ce4b-4129-8c89-930d26ae0f6a",
+                        "exec": [test_script],
+                        "type": "text/javascript"
+                    }
+                }
+            ]
+        }
+    else:
+        data = {
+            "name": request_name,
+            "url": request_url,
+            "method": request_method,
+            "headers": request_headers,
+            "dataMode": "raw",
+            "rawModeData": json.dumps(request_body, indent=2),
+            "dataOptions": {
+                "raw": {
+                    "language": "json"
+                }
+            },
+            "events": [
+                {
+                    "listen": "test",
+                    "script": {
+                        "id": "a8608e1a-ce4b-4129-8c89-930d26ae0f6a",
+                        "exec": [test_script],
+                        "type": "text/javascript"
+                    }
+                }
+            ]
+        }
+    
+    response = requests.post(url, headers=headers, json=data)
+    print(response)
+    return response
+
+def edit_and_create_get_requests(api_key, collection_id, folder_id, file_path, request_method, request_headers, test_script):
+    original_url = read_curl_file(file_path)
+    parsed_params = parse_query_params(original_url)
+    edited_query_string = edit_query_params(parsed_params)
+    base_url = original_url.split('?')[0]
+    edited_url = f"{base_url}?{edited_query_string}"
+    
+    request_name = "Edited GET Request"
+
+
+    create_request(api_key, collection_id, folder_id, request_name, request_method, request_headers, None, edited_url, test_script)
--- qastronaut.py
+++ qastronaut.py
@@ -1,13 +1,14 @@
-from functions.create_postman import create_collection, create_folder, create_test_empty, create_test_null, create_test_nonexistent, create_test_invalid, create_test_lenght
+from functions.create_postman import create_collection, create_folder, create_test_empty, create_test_null, create_test_nonexistent, create_test_invalid
+from functions.create_postman_GET import edit_and_create_get_requests  # Assegure-se de que o caminho está correto
 from functions.fetch_data_postman import extract_curl_data
-from functions.welcome import welcome, names, get_user_request_names,lost_api_key
+from functions.welcome import welcome, names, get_user_request_names, lost_api_key
 import os
 import argparse
 import pyfiglet
 
-green_color = "\033[1;32m" 
+green_color = "\033[1;32m"
 blue_color = "\033[1;34m"
-reset_color = "\033[0m" 
+reset_color = "\033[0m"
 
 text = "QAstronaut"
 banner = pyfiglet.figlet_format(text)
@@ -26,20 +27,20 @@ else:
     api_key = lost_api_key()
 
 print(f"\nPlease put the curl command in a text file named '{blue_color}config/requests/curl.txt{reset_color}' and then press '{blue_color}Enter{reset_color}'.")
-    
+
 user_input = input(f"\nDid you place the curl command in '{blue_color}curl.txt{reset_color}'? (Y/n): ")
-   
+
 with open('config/requests/curl.txt', 'r') as file:
-        validate_curl = file.read()
+    validate_curl = file.read()
 
 if validate_curl == "Paste your cURL here":
     print(f"\nPut a valid curl command in '{blue_color}config/requests/curl.txt{reset_color}' and try again.")
-    exit() 
+    exit()
 
 if user_input.strip().lower() not in ["y", ""]:
     print(f"\nPlease put the curl command in '{blue_color}config/requests/curl.txt{reset_color}' and try again.")
     exit()
-    
+
 curl_file_path = os.path.join('config/requests', 'curl.txt')
 requests_name_arch = 'config/requests'
 name_file_path = os.path.join(requests_name_arch, 'user_requests')
@@ -61,7 +62,7 @@ folder_id = create_folder(collection_id, folder_name, api_key)
 
 print(f"\nRequest Method: {green_color}{request_method}{reset_color}")
 print(f"Request URL: {green_color}{request_url}{reset_color}")
-print(f"Request Body: {green_color}{request_body}{reset_color}")
+print(f"Request Body: {green_color}{request_body if request_body else 'None'}{reset_color}")
 print(f"Request Headers: {green_color}{request_headers}{reset_color}")
 
 runner = input("\nCan I run? (Y/n): ")
@@ -73,31 +74,16 @@ print('\n-----------------------------------------------------------------------
 
 user_request_names = get_user_request_names()
 
-if request_body is None:
-
-    # teste de get
-
-    # realizei esse import por não ter um create_test_empty_get por enquanto
-    from functions.create_postman import create_request
-
-    # essa variável foi coloca dentro de cada create_test, como a função não foi criada ainda ela fica por aqui
-    with open('config/tests/params/null', 'r') as file:
-        test_script = file.read()
-
-    new_request_test = create_request(api_key, collection_id, folder_id, user_request_names, request_method, request_headers, request_body, request_url, test_script)
-    print("Create request GET sucess")
-
-    # fim teste de get
-
+if request_method == "GET":
+    edit_and_create_get_requests(api_key, collection_id, folder_id, curl_file_path, request_method, request_headers, "console.log('Test passed')")  # Exemplo de script de teste
+    print("Create request GET success")
 else:
-
     new_request_empty = create_test_empty(api_key, collection_id, folder_id, user_request_names, request_method, request_headers, request_body, request_url)
     print('----------------------------------------------------------------------------------------------------------------------------')
     new_request_null = create_test_null(api_key, collection_id, folder_id, user_request_names, request_method, request_headers, request_body, request_url)
     print('----------------------------------------------------------------------------------------------------------------------------')
-    new_request_noneexistent = create_test_nonexistent(api_key, collection_id, folder_id, user_request_names, request_method, request_headers, request_body, request_url)
+    new_request_nonexistent = create_test_nonexistent(api_key, collection_id, folder_id, user_request_names, request_method, request_headers, request_body, request_url)
     print('----------------------------------------------------------------------------------------------------------------------------')
     new_request_invalid = create_test_invalid(api_key, collection_id, folder_id, user_request_names, request_method, request_headers, request_body, request_url)
     print('----------------------------------------------------------------------------------------------------------------------------')
-    new_request_lenght = create_test_lenght(api_key, collection_id, folder_id, user_request_names, request_method, request_headers, request_body, request_url)
-    print(f"---------------------------------------------------{blue_color}End{reset_color}----------------------------------------------------------------------")
+    # new_request_length = create_test_length(api_key, collection_id, folder_id, user_request_names, request_method, request_headers, request_body, request)
